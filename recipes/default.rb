a1bc1cebf063104c3c17b4deca76296a3af14dae
Andrew Bruce <andrew@camelpunch.com>
2013-12-31 17:44:34 -0800

    fix recipe name
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,4 @@ include_recipe 'chef_rubies'
 
 include_recipe 'cf-jenkins::jenkins_base'
 include_recipe 'cf_pipeline::pipelines'
-include_recipe 'cf_pipeline::pipeline_jenkins_plugins'
+include_recipe 'cf_pipeline::jenkins_plugins'
