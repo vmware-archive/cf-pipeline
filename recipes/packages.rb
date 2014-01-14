89e435916b29fa1cb4b69b79c6b259956f8ee18f
Andrew Bruce <andrew@camelpunch.com>
2014-01-13 16:19:02 -0800

    Allow user to customize packages
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,2 +1,4 @@
 package 'libxml2-dev'
 package 'libxslt-dev'
+
+node['cf_pipeline']['packages'].each {|name| package name}
--- spec/cf_pipeline/recipes/packages_spec.rb
+++ spec/cf_pipeline/recipes/packages_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 describe 'cf_pipeline::packages' do
   subject(:chef_run) do
     ChefSpec::Runner.new do |node|
+      node.set['cf_pipeline']['packages'] = %w(not_a_real_package)
     end.converge(described_recipe)
   end
 
   it "installs libxml2-dev and libxslt-dev for Nokogiri" do
     expect(chef_run).to install_package('libxml2-dev')
     expect(chef_run).to install_package('libxslt-dev')
+  end
+
+  it "installs any packages named in the node attributes" do
+    expect(chef_run).to install_package('not_a_real_package')
   end
 end
