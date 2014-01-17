e8512ed1a2fe9ccba634184500c920821b48a9ef
Dan Wendorf <dwendorf@pivotallabs.com>
2014-01-17 11:31:58 -0800

    git submodule init before update
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- attributes/cf_deployer_ref.rb
+++ attributes/cf_deployer_ref.rb
@@ -1,1 +1,1 @@
-node.default['cf_pipeline']['cf_deployer_ref'] = '354ba563e13addf1ba3e2de8b0e1426cd91a140f'
+node.default['cf_pipeline']['cf_deployer_ref'] = 'fd22f403f24f53881d12fee463eb8476331d4426'
