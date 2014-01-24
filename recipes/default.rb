a986ce9b866a940ddd75991b17f8ceff2b67e229
Dan Wendorf <dwendorf@pivotallabs.com>
2014-01-23 17:35:01 -0800

    Copy SSH keys *after* target folder is available
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,11 +3,11 @@ include_recipe 'apt'
 include_recipe 'cf-jenkins::gvm'
 include_recipe 'chef_rubies'
 include_recipe 'cf_pipeline::packages'
-include_recipe 'cf_pipeline::ssh'
 
 include_recipe 'cf_pipeline::pipeline_scripts'
 
 include_recipe 'cf-jenkins::jenkins_base'
+include_recipe 'cf_pipeline::ssh'
 include_recipe 'cf_pipeline::jenkins_config'
 include_recipe 'cf_pipeline::disable_nginx_default_site'
 include_recipe 'cf_pipeline::pipelines'
